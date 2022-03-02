19a3acee6a289473612bc262880d66c0e3ca0c6e
seyyitgenc <mahluk053@gmail.com>
2022-03-02 21:55:36 +0300

    camera  and bird controls update

--- Src/Actors/Bird.gd
+++ Src/Actors/Bird.gd
@@ -4,8 +4,12 @@ const gravity=10
 const up = Vector2(0,-1)
 const flap = 200
 const maxfallspeed = 200
-
+export var turning = 70
 var point: = 0
+var rot_degree=1
+var is_falling = true
+
+onready var anim_player:AnimationPlayer=get_node("AnimationPlayer")
 
 var Wall = preload("res://Src/Levels/Walls.tscn")
 
@@ -17,17 +21,26 @@ func _ready():
 # warning-ignore:unused_argument
 func _physics_process(delta):
 	motion.y += gravity
+	rot_degree-=1
+	is_falling=true
 	if motion.y > maxfallspeed:
 		motion.y = maxfallspeed
 	
+	if is_falling == true:
+		if rot_degree<0 and rot_degree >-45:
+			$FlappyBird.rotation_degrees+=delta*turning
+		else:
+			rot_degree=0
 	if  Input.is_action_just_pressed("boost"):
 		motion.y = -flap
+		anim_player.play("Boost")
+		
 	motion = move_and_slide(motion,up)
-	get_parent().get_node("Bird/Camera2D/RichTextLabel").text=str(point)
+	get_parent().get_node("RichTextLabel").text=str(point)
 
 func wall_reset():
 	var wall_instance = Wall.instance()
-	wall_instance.position=Vector2(880,rand_range(-180,60))
+	wall_instance.position=Vector2(440,rand_range(-180,60))
 	get_parent().call_deferred("add_child",wall_instance)
 
 
