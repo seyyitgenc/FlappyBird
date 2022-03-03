fe5f48474fa6c3ac1000e3d4477549c625b73a90
seyyitgenc <mahluk053@gmail.com>
2022-03-03 17:41:14 +0300

    animation bug fix

--- Src/Actors/Bird.gd
+++ Src/Actors/Bird.gd
@@ -4,10 +4,11 @@ const gravity=10
 const up = Vector2(0,-1)
 const flap = 200
 const maxfallspeed = 200
-export var turning = 100
+
 var point: = 0
-var rot_degree=1
-var is_falling = true
+var rot_downward = 1
+var turning = 10
+var rot_upward = -1
 
 onready var anim_player:AnimationPlayer=get_node("AnimationPlayer")
 
@@ -21,22 +22,20 @@ func _ready():
 # warning-ignore:unused_argument
 func _physics_process(delta):
 	motion.y += gravity
-	rot_degree-=1
-	is_falling=true
 	if motion.y > maxfallspeed:
 		motion.y = maxfallspeed
 	
-	if is_falling == true:
-		if rot_degree<0 and rot_degree >-45:
-			$FlappyBird.rotation_degrees+=delta*turning
-		else:
-			rot_degree=0
+	if rot_downward > 0 and rot_downward<60:
+		rot_downward+=1
+		$FlappyBird.rotation+=delta*2
+	
 	if  Input.is_action_just_pressed("boost"):
 		motion.y = -flap
+		anim_player.stop()
 		anim_player.play("Boost")
-		
+		rot_downward=1
 	motion = move_and_slide(motion,up)
-	get_parent().get_node("RichTextLabel").text=str(point)
+	get_parent().get_parent().get_node("Point/RichTextLabel").text=str(point)
 
 func wall_reset():
 	var wall_instance = Wall.instance()
@@ -51,13 +50,18 @@ func _on_Detect_area_entered(area):
 
 
 func _on_Detect_body_entered(body):
-	if body.name=="Walls":
+	if body.name =="Walls":
+# warning-ignore:return_value_discarded
 		get_tree().reload_current_scene()
 	
 
-
-
 func _on_Resetter_body_entered(body):
-	if body.name== "Walls":
+	if body.name == "Walls":
 		body.queue_free()
 		wall_reset()
+
+
+func _on_Limiter_area_entered(area):
+	if area.name=="Detect":
+# warning-ignore:return_value_discarded
+		get_tree().reload_current_scene()
